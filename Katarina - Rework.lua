df1442edf2150b8ee42bfe6d7ad06520b48df73d
Jaikor <pedrodsp@gmail.com>
2016-03-06 11:24:57 -0400

    Update Katarina - Rework.lua
    Fixed issue for Free users and added Auto Updater

--- Katarina - Rework.lua
+++ Katarina - Rework.lua
@@ -3,14 +3,18 @@ if myHero.charName ~= 'Katarina' then return end
 assert(load(Base64Decode("G0x1YVIAAQQEBAgAGZMNChoKAAAAAAAAAAAAAQQfAAAAAwAAAEQAAACGAEAA5QAAAJ1AAAGGQEAA5UAAAJ1AAAGlgAAACIAAgaXAAAAIgICBhgBBAOUAAQCdQAABhkBBAMGAAQCdQAABhoBBAOVAAQCKwICDhoBBAOWAAQCKwACEhoBBAOXAAQCKwICEhoBBAOUAAgCKwACFHwCAAAsAAAAEEgAAAEFkZFVubG9hZENhbGxiYWNrAAQUAAAAQWRkQnVnc3BsYXRDYWxsYmFjawAEDAAAAFRyYWNrZXJMb2FkAAQNAAAAQm9sVG9vbHNUaW1lAAQQAAAAQWRkVGlja0NhbGxiYWNrAAQGAAAAY2xhc3MABA4AAABTY3JpcHRUcmFja2VyAAQHAAAAX19pbml0AAQSAAAAU2VuZFZhbHVlVG9TZXJ2ZXIABAoAAABzZW5kRGF0YXMABAsAAABHZXRXZWJQYWdlAAkAAAACAAAAAwAAAAAAAwkAAAAFAAAAGABAABcAAIAfAIAABQAAAAxAQACBgAAAHUCAAR8AgAADAAAAAAQSAAAAU2VuZFZhbHVlVG9TZXJ2ZXIABAcAAAB1bmxvYWQAAAAAAAEAAAABAQAAAAAAAAAAAAAAAAAAAAAEAAAABQAAAAAAAwkAAAAFAAAAGABAABcAAIAfAIAABQAAAAxAQACBgAAAHUCAAR8AgAADAAAAAAQSAAAAU2VuZFZhbHVlVG9TZXJ2ZXIABAkAAABidWdzcGxhdAAAAAAAAQAAAAEBAAAAAAAAAAAAAAAAAAAAAAUAAAAHAAAAAQAEDQAAAEYAwACAAAAAXYAAAUkAAABFAAAATEDAAMGAAABdQIABRsDAAKUAAADBAAEAXUCAAR8AgAAFAAAABA4AAABTY3JpcHRUcmFja2VyAAQSAAAAU2VuZFZhbHVlVG9TZXJ2ZXIABAUAAABsb2FkAAQMAAAARGVsYXlBY3Rpb24AAwAAAAAAQHpAAQAAAAYAAAAHAAAAAAADBQAAAAUAAAAMAEAAgUAAAB1AgAEfAIAAAgAAAAQSAAAAU2VuZFZhbHVlVG9TZXJ2ZXIABAgAAAB3b3JraW5nAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAEBAAAAAAAAAAAAAAAAAAAAAAAACAAAAA0AAAAAAAYyAAAABgBAAB2AgAAaQEAAF4AAgEGAAABfAAABF0AKgEYAQQBHQMEAgYABAMbAQQDHAMIBEEFCAN0AAAFdgAAACECAgUYAQQBHQMEAgYABAMbAQQDHAMIBEMFCAEbBQABPwcICDkEBAt0AAAFdgAAACEAAhUYAQQBHQMEAgYABAMbAQQDHAMIBBsFAAA9BQgIOAQEARoFCAE/BwgIOQQEC3QAAAV2AAAAIQACGRsBAAIFAAwDGgEIAAUEDAEYBQwBWQIEAXwAAAR8AgAAOAAAABA8AAABHZXRJbkdhbWVUaW1lcgADAAAAAAAAAAAECQAAADAwOjAwOjAwAAQGAAAAaG91cnMABAcAAABzdHJpbmcABAcAAABmb3JtYXQABAYAAAAlMDIuZgAEBQAAAG1hdGgABAYAAABmbG9vcgADAAAAAAAgrEAEBQAAAG1pbnMAAwAAAAAAAE5ABAUAAABzZWNzAAQCAAAAOgAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAA4AAAATAAAAAAAIKAAAAAEAAABGQEAAR4DAAIEAAAAhAAiABkFAAAzBQAKAAYABHYGAAVgAQQIXgAaAR0FBAhiAwQIXwAWAR8FBAhkAwAIXAAWARQGAAFtBAAAXQASARwFCAoZBQgCHAUIDGICBAheAAYBFAQABTIHCAsHBAgBdQYABQwGAAEkBgAAXQAGARQEAAUyBwgLBAQMAXUGAAUMBgABJAYAAIED3fx8AgAANAAAAAwAAAAAAAPA/BAsAAABvYmpNYW5hZ2VyAAQLAAAAbWF4T2JqZWN0cwAECgAAAGdldE9iamVjdAAABAUAAAB0eXBlAAQHAAAAb2JqX0hRAAQHAAAAaGVhbHRoAAQFAAAAdGVhbQAEBwAAAG15SGVybwAEEgAAAFNlbmRWYWx1ZVRvU2VydmVyAAQGAAAAbG9vc2UABAQAAAB3aW4AAAAAAAMAAAAAAAEAAQEAAAAAAAAAAAAAAAAAAAAAFAAAABQAAAACAAICAAAACkAAgB8AgAABAAAABAoAAABzY3JpcHRLZXkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFAAAABUAAAACAAUKAAAAhgBAAMAAgACdgAABGEBAARfAAICFAIAAjIBAAQABgACdQIABHwCAAAMAAAAEBQAAAHR5cGUABAcAAABzdHJpbmcABAoAAABzZW5kRGF0YXMAAAAAAAIAAAAAAAEBAAAAAAAAAAAAAAAAAAAAABYAAAAlAAAAAgATPwAAAApAAICGgEAAnYCAAAqAgICGAEEAxkBBAAaBQQAHwUECQQECAB2BAAFGgUEAR8HBAoFBAgBdgQABhoFBAIfBQQPBgQIAnYEAAcaBQQDHwcEDAcICAN2BAAEGgkEAB8JBBEECAwAdggABFgECAt0AAAGdgAAACoCAgYaAQwCdgIAACoCAhgoAxIeGQEQAmwAAABdAAIAKgMSHFwAAgArAxIeGQEUAh4BFAQqAAIqFAIAAjMBFAQEBBgBBQQYAh4FGAMHBBgAAAoAAQQIHAIcCRQDBQgcAB0NAAEGDBwCHw0AAwcMHAAdEQwBBBAgAh8RDAFaBhAKdQAACHwCAACEAAAAEBwAAAGFjdGlvbgAECQAAAHVzZXJuYW1lAAQIAAAAR2V0VXNlcgAEBQAAAGh3aWQABA0AAABCYXNlNjRFbmNvZGUABAkAAAB0b3N0cmluZwAEAwAAAG9zAAQHAAAAZ2V0ZW52AAQVAAAAUFJPQ0VTU09SX0lERU5USUZJRVIABAkAAABVU0VSTkFNRQAEDQAAAENPTVBVVEVSTkFNRQAEEAAAAFBST0NFU1NPUl9MRVZFTAAEEwAAAFBST0NFU1NPUl9SRVZJU0lPTgAECwAAAGluZ2FtZVRpbWUABA0AAABCb2xUb29sc1RpbWUABAYAAABpc1ZpcAAEAQAAAAAECQAAAFZJUF9VU0VSAAMAAAAAAADwPwMAAAAAAAAAAAQJAAAAY2hhbXBpb24ABAcAAABteUhlcm8ABAkAAABjaGFyTmFtZQAECwAAAEdldFdlYlBhZ2UABA4AAABib2wtdG9vbHMuY29tAAQXAAAAL2FwaS9ldmVudHM/c2NyaXB0S2V5PQAECgAAAHNjcmlwdEtleQAECQAAACZhY3Rpb249AAQLAAAAJmNoYW1waW9uPQAEDgAAACZib2xVc2VybmFtZT0ABAcAAAAmaHdpZD0ABA0AAAAmaW5nYW1lVGltZT0ABAgAAAAmaXNWaXA9AAAAAAACAAAAAAABAQAAAAAAAAAAAAAAAAAAAAAmAAAAKgAAAAMACiEAAADGQEAAAYEAAN2AAAHHwMAB3YCAAArAAIDHAEAAzADBAUABgACBQQEA3UAAAscAQADMgMEBQcEBAIABAAHBAQIAAAKAAEFCAgBWQYIC3UCAAccAQADMgMIBQcECAIEBAwDdQAACxwBAAMyAwgFBQQMAgYEDAN1AAAIKAMSHCgDEiB8AgAASAAAABAcAAABTb2NrZXQABAgAAAByZXF1aXJlAAQHAAAAc29ja2V0AAQEAAAAdGNwAAQIAAAAY29ubmVjdAADAAAAAAAAVEAEBQAAAHNlbmQABAUAAABHRVQgAAQSAAAAIEhUVFAvMS4wDQpIb3N0OiAABAUAAAANCg0KAAQLAAAAc2V0dGltZW91dAADAAAAAAAAAAAEAgAAAGIAAwAAAPyD15dBBAIAAAB0AAQKAAAATGFzdFByaW50AAQBAAAAAAQFAAAARmlsZQAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAABAAAAAAAAAAAAAAAAAAAAAAA="), nil, "bt", _ENV))()
 TrackerLoad("FTqjCPEE6ycKT2fq")
 
-local KatarinaVersion = 3.35
+--local KatarinaVersion = 3.36
 
 --|> Cuz superx is the only lua bender
 require 'SxOrbWalk'
 
 class 'Katarina'
+class 'Download'
 
 	function Katarina:__init()
+         --|> Version checker and updater
+        self.Version = 3.37
+        self:CheckUpdates()
 		--|> Spell Information
 		self.spells = {
 			Q = Spells(_Q, 675, 'Bouncing Blades', 'targeted', ARGB(255,178, 0 , 0 )),
@@ -84,7 +88,7 @@ class 'Katarina'
 		AddDeleteObjCallback(function(obj) self:ObjDelete(obj) end)
 
 		--|> Prints Loaded
-		print("<font color=\"#FF0000\">[Nintendo Katarina]:</font> <font color=\"#FFFFFF\">Loaded Version: "..KatarinaVersion.."</font>")
+		print("<font color=\"#FF0000\">[Nintendo Katarina]:</font> <font color=\"#FFFFFF\">Loaded Version: "..self.Version.."</font>")
 	end
 
 
@@ -179,7 +183,9 @@ class 'Katarina'
 	end
 
 	function Katarina:Tick()
+    if VIP_USER then
     SetSkin(myHero, self.menu.other.skins - 1)
+    end
 		self.target = self:GetTarget()
 		if self.target  and not self.using then
 			if self.menu.comboKey then
@@ -855,6 +861,185 @@ class 'Update'
 		end
 	end
 ]]--
+
+
+function Katarina:CheckUpdates()
+	host = "www.scarjit.de"
+	file = "/Jaikor/BoL/Scripts/Katarina%20-%20Rework.lua"
+	name = GetCurrentEnv().FILE_NAME
+	versionfilepath = "/Jaikor/BoL/Versions/Katarina.version"
+	path = SCRIPT_PATH
+	DL = Download()
+	DL:downloadUpdate(host, file, name, path, function ()
+	print("Downloaded update, press F9 2x to reload")
+	end, self.Version, versionfilepath)
+end
+    
+class "Download"
+function Download:__init()
+	socket = require("socket")
+	self.aktivedownloads = {}
+	self.callbacks = {}
+
+	AddTickCallback(function ()
+		self:RemoveDone()
+	end)
+
+	class("Async")
+	function Async:__init(host, filepath, localname, drawoffset, localpath)
+		self.progress = 0
+		self.host = host
+		self.filepath = filepath
+		self.localname = localname
+		self.offset = drawoffset
+		self.localpath = localpath
+		self.CRLF = '\r\n'
+
+		self.headsocket = socket.tcp()
+		self.headsocket:settimeout(1)
+		self.headsocket:connect(self.host, 80)
+		self.headsocket:send('HEAD '..self.filepath..' HTTP/1.1'.. self.CRLF ..'Host: '..self.host.. self.CRLF ..'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'.. self.CRLF .. self.CRLF)
+
+		self.HEADdata = ""
+		self.DLdata = ""
+		self.StartedDownload = false
+		self.canDL = true
+
+		AddTickCallback(function ()
+			self:tick()
+		end)
+		AddDrawCallback(function ()
+			self:draw()
+		end)
+	end
+
+	function Async:tick()
+		if self.progress == 100 then return end
+		if self.HEADcStatus ~= "timeout" and self.HEADcStatus ~= "closed" then
+			self.HEADfString, self.HEADcStatus, self.HEADpString = self.headsocket:receive(16);
+			if self.HEADfString then
+				self.HEADdata = self.HEADdata..self.HEADfString
+			elseif self.HEADpString and #self.HEADpString > 0 then
+				self.HEADdata = self.HEADdata..self.HEADpString
+			end
+		elseif self.HEADcStatus == "timeout" then
+			self.headsocket:close()
+			--Find Lenght
+			local begin = string.find(self.HEADdata, "Length: ")
+			if begin then
+				self.HEADdata = string.sub(self.HEADdata,begin+8)
+				local n = 0
+				local _break = false
+				for i=1, #self.HEADdata do
+					local c = tonumber(string.sub(self.HEADdata,i,i))
+					if c and _break == false then
+						n = n+1
+					else
+						_break = true
+					end
+				end
+				self.HEADdata = string.sub(self.HEADdata,1,n)
+				self.StartedDownload = true
+				self.HEADcStatus = "closed"
+			end
+		end
+		if self.HEADcStatus == "closed" and self.StartedDownload == true and self.canDL == true then --Double Check
+			self.canDL = false
+			self.DLsocket = socket.tcp()
+			self.DLsocket:settimeout(1)
+			self.DLsocket:connect(self.host, 80)
+			--Start Main Download
+			self.DLsocket:send('GET '..self.filepath..' HTTP/1.1'.. self.CRLF ..'Host: '..self.host.. self.CRLF ..'User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'.. self.CRLF .. self.CRLF)
+		end
+		
+		if self.DLsocket and self.DLcStatus ~= "timeout" and self.DLcStatus ~= "closed" then
+			self.DLfString, self.DLcStatus, self.DLpString = self.DLsocket:receive(1024);
+			
+			if ((self.DLfString) or (self.DLpString and #self.DLpString > 0)) then
+				self.DLdata = self.DLdata .. (self.DLfString or self.DLpString)
+			end
+
+		elseif self.DLcStatus and self.DLcStatus == "timeout" then
+			self.DLsocket:close()
+			self.DLcStatus = "closed"
+			self.DLdata = string.sub(self.DLdata,#self.DLdata-tonumber(self.HEADdata)+1)
+
+			local file = io.open(self.localpath.."\\"..self.localname, "w+b")
+			file:write(self.DLdata)
+			file:close()
+			self.progress = 100
+		end
+
+		if self.progress ~= 100 and self.DLdata and #self.DLdata > 0 then
+			self.progress = (#self.DLdata/tonumber(self.HEADdata))*100
+		end
+	end
+
+	function Async:draw()
+		if self.progress < 100 then
+			DrawTextA("Downloading: "..self.localname,15,50,35+self.offset)
+			DrawRectangleOutline(49,50+self.offset,250,20, ARGB(255,255,255,255),1)
+			if self.progress ~= 100 then
+				DrawLine(50,60+self.offset,50+(2.5*self.progress),60+self.offset,18,ARGB(150,255-self.progress*2.5,self.progress*2.5,255-self.progress*2.5))
+				DrawTextA(tostring(math.round(self.progress).." %"), 15,150,52+self.offset)
+			end
+		end
+	end
+
+end
+
+function Download:newDL(host, file, name, path, callback)
+	local offset = (#self.aktivedownloads+1)*40
+	self.aktivedownloads[#self.aktivedownloads+1] = Async(host, file, name, offset-40, path)
+	if not callback then
+		callback = (function ()
+		end)
+	end
+
+	self.callbacks[#self.callbacks+1] = callback
+
+end
+
+function Download:RemoveDone()
+	if #self.aktivedownloads == 0 then return end
+	local x = {}
+	local c = {}
+	for k, v in pairs(self.aktivedownloads) do
+		if math.round(v.progress) < 100 then
+			v.offset = k*40-40
+			x[#x+1] = v
+			c[#c+1] = self.callbacks[k]
+		else
+			self.callbacks[k]()
+			self.callbacks[k] = function ()
+				
+			end
+		end
+	end
+	self.callbacks = {}
+	self.callbacks = c
+	self.aktivedownloads = {}
+	self.aktivedownloads = x
+end
+
+function Download:downloadUpdate(host, file, name, path, _callback,version, versionfilepath)
+	function HasUpdate(name,cb)
+		function _cb()
+			cb()
+			DeleteFile(name..".version")
+		end
+		DelayAction(function ()
+			local v = ReadFile(name..".version")
+			if v and tonumber(v) > tonumber(version) then
+					self:newDL(host, file, name, path, cb)
+			end
+		end,0.25)
+	end
+	self:newDL(host,versionfilepath,name..".version",path,function ()
+		HasUpdate(name,_callback)
+	end)
+
+end
     
 --|> Self Initiation
 function OnLoad()
